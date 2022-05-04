f3d17180cbb3e2927defa48f9d4d3b275af34f5f
Miguel Angel Saravia Belmonte <miguel.saravia@elektra.com.mx>
2022-05-04 19:07:17 -0400

    create post controller

--- Zemoga/Zemoga/View/PostViewController.swift
+++ Zemoga/Zemoga/View/PostViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PostViewController.swift
 //  Zemoga
 //
 //  Created by Miguel Angel Saravia Belmonte on 5/4/22.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PostViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        navigationItem.title = "Posts"
     }
-
-
 }
-
